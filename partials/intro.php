258bee9204e1ca3c871fd6934aa5dd62b6a94d8f
Freelancer2 <esiuser@freelancer2.esidesign.com>
2018-02-05 17:14:00 -0500

    testing:

--- partials/intro.php
+++
@@ -1,6 +0,0 @@
-<div id="intro">
-	<?php
-		$content = get_svg("assets/icons/shift-logo.svg");
-		include("partials/brand-animation.php");
-	?>
-</div>
--- partials/link-fallback.php
+++
@@ -1,3 +0,0 @@
-<div id="link">
-	<p class="serif med-text uppercase">Open to all tenants / Learn more at shiftbeyondwork.com</p>
-</div>
